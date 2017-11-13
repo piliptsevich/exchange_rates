7c152332487258b83a116f93c56c5b4fc33314b1
piliptsevich <piliptsevichalex@gmail.com>
2017-11-13 14:24:50 +0100

    beautify code

--- js/display_item.js
+++ js/display_item.js
@@ -1,39 +1,37 @@
-$(document).ready(function() {
-  function showRate(checkboxId){
-    var className = checkboxId;
-    // var id = checkboxId.substring(0, checkboxId.length-1);
-    if(document.getElementById(checkboxId).checked == false){
-      var element = document.getElementsByClassName(className);
-      element[0].style.display = "none";
+function showRate(checkboxId){
+  var className = checkboxId;
+  // var id = checkboxId.substring(0, checkboxId.length-1);
+  if(document.getElementById(checkboxId).checked == false){
+    var element = document.getElementsByClassName(className);
+    element[0].style.display = "none";
+  }
+  else{
+    var element = document.getElementsByClassName(className);
+    element[0].style.display = "inline-block";
+  }
+}
+
+function openNav() {
+  if($(window).width() > 992){
+    document.getElementById("mySidenav").style.width = "25%";
+  }
+  else{
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+  function changeColor(classList){
+    var element = document.getElementById("content");
+    if(classList[1] == "dark-theme"){
+      element.classList.remove("white-theme");
+      element.classList.add("dark-theme");
     }
-    else{
-      var element = document.getElementsByClassName(className);
-      element[0].style.display = "inline-block";
+    if(classList[1] == "white-theme"){
+      element.classList.remove("dark-theme");
+      element.classList.add("white-theme");
     }
   }
-
-  function openNav() {
-    if($(window).width() > 992){
-      document.getElementById("mySidenav").style.width = "25%";
-    }
-    else{
-      document.getElementById("mySidenav").style.width = "100%";
-    }
-  }
-
-  function closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
-  }
-
-    function changeColor(classList){
-      var element = document.getElementById("content");
-      if(classList[1] == "dark-theme"){
-        element.classList.remove("white-theme");
-        element.classList.add("dark-theme");
-      }
-      if(classList[1] == "white-theme"){
-        element.classList.remove("dark-theme");
-        element.classList.add("white-theme");
-      }
-    }
-});
--- js/set_rates.js
+++ js/set_rates.js
@@ -1,144 +1,144 @@
 $(document).ready(function() {
 
-var apiPath1 = "https://api.fixer.io/latest?base=";
-var apiPath2 = "https://api.coindesk.com/v1/bpi/currentprice/USD.json";
-var apiPath3 = "https://api.coindesk.com/v1/bpi/currentprice.json";
+	var apiPath1 = "https://api.fixer.io/latest?base=";
+	var apiPath2 = "https://api.coindesk.com/v1/bpi/currentprice/USD.json";
+	var apiPath3 = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
-var lastRatesArray = [0,0,0,0,0,0,0,0];
+	var lastRatesArray = [0,0,0,0,0,0,0,0];
 
-  function set_rates(element_id, apiPath, base, quote){
+	function set_rates(element_id, apiPath, base, quote){
 
-	switch(element_id) {
-		// Rates API for RUB
-	    case "#EURRUB":
-	        var api = apiPath + base;
+		switch(element_id) {
+			// Rates API for RUB
+		    case "#EURRUB":
+		        var api = apiPath + base;
 
-		  	$.getJSON(api).then(function(json){
-					var currencyData = json;
-					var newRates = currencyData.rates[quote];
-					$(element_id).text(newRates);
+			  	$.getJSON(api).then(function(json){
+						var currencyData = json;
+						var newRates = currencyData.rates[quote];
+						$(element_id).text(newRates);
 
-					if(newRates > lastRatesArray[0]){
-						$(element_id).css('color', '#09b83e');
-						// console.log("* " + newRates);
-					}
-					else if(newRates < lastRatesArray[0]){
-						$(element_id).css('color', '#cd201f');
-						// console.log("* " + newRates);
-					}
-					lastRatesArray[0] = newRates;
-			});	      
-	        break;
-		// Rates API for RUB
-	    case "#USDRUB":
-	        var api = apiPath + base;
+						if(newRates > lastRatesArray[0]){
+							$(element_id).css('color', '#09b83e');
+							// console.log("* " + newRates);
+						}
+						else if(newRates < lastRatesArray[0]){
+							$(element_id).css('color', '#cd201f');
+							// console.log("* " + newRates);
+						}
+						lastRatesArray[0] = newRates;
+				});	      
+		        break;
+			// Rates API for RUB
+		    case "#USDRUB":
+		        var api = apiPath + base;
 
-		  	$.getJSON(api).then(function(json){
-					var currencyData = json;
-					var newRates = currencyData.rates[quote];
-					$(element_id).text(newRates);
+			  	$.getJSON(api).then(function(json){
+						var currencyData = json;
+						var newRates = currencyData.rates[quote];
+						$(element_id).text(newRates);
 
-					if(newRates > lastRatesArray[1]){
-						$(element_id).css('color', '#09b83e');
-						// console.log("* " + newRates);
-					}
-					else if(newRates < lastRatesArray[1]){
-						$(element_id).css('color', '#cd201f');
-						// console.log("* " + newRates);
-					}
-					lastRatesArray[1] = newRates;
-			});	        
-	        break;
-		// Rates API for BTC
-	    case "#BTCUSD":
-	    	var api = apiPath;
-
-	        $.getJSON(api).then(function(json){
-	        		
-					var currencyData = json;
-					var newRates = (currencyData.bpi.USD.rate_float);
-					$(element_id).text(newRates);
-
-					if(newRates > lastRatesArray[2]){
-						$(element_id).css('color', '#09b83e');
-						// console.log("* " + newRates);
-					}
-					else if(newRates < lastRatesArray[2]){
-						$(element_id).css('color', '#cd201f');
-						// console.log("* " + newRates);
-					}
-					lastRatesArray[2] = newRates;
-			});	  
-	        break;
-		// Rates API for EUR/USD
-	    case "#EURUSD":
-	    	var api = apiPath;
-
-	        $.getJSON(api).then(function(json){
-	        		
-					var currencyData = json;
-					var newRates = ((currencyData.bpi.USD.rate_float)/(currencyData.bpi.EUR.rate_float)).toFixed(5);
-					$(element_id).text(newRates);
-
-					if(newRates > lastRatesArray[3]){
-						$(element_id).css('color', '#09b83e');
-						// console.log("* " + newRates);
-					}
-					else if(newRates < lastRatesArray[3]){
-						$(element_id).css('color', '#cd201f');
-						// console.log("* " + newRates);
-					}
-					lastRatesArray[3] = newRates;
-			});	  
-	        break;
-		// 	// // Rates API for EUR/JPY
-		//  //    case "#EURJPY" & apiPath:
-		//  //        $.getJSON(apiPath).then(function(json){
-		        		
-		// 	// 			var currencyData = json;
-		// 	// 			// var lastRates;
-		// 	// 			var newRates = (currencyData.value);
-		// 	// 			$(element_id).text(newRates);
-
-		// 	// 			if(newRates >= lastRates){
-		// 	// 				$(element_id).css('color', '#09b83e');
-		// 	// 				// console.log("* " + newRates);
-		// 	// 			}
-		// 	// 			else if(newRates < lastRates){
-		// 	// 				$(element_id).css('color', '#cd201f');
-		// 	// 				// console.log("* " + newRates);
-		// 	// 			}
-		// 	// 			lastRates = newRates;
-		// 	// 	});	  
-		//  //        break;
-        		// Rates API for EUR/GBP
-			    case "#EURGBP":
+						if(newRates > lastRatesArray[1]){
+							$(element_id).css('color', '#09b83e');
+							// console.log("* " + newRates);
+						}
+						else if(newRates < lastRatesArray[1]){
+							$(element_id).css('color', '#cd201f');
+							// console.log("* " + newRates);
+						}
+						lastRatesArray[1] = newRates;
+				});	        
+		        break;
+			// Rates API for BTC
+		    case "#BTCUSD":
 		    	var api = apiPath;
 
 		        $.getJSON(api).then(function(json){
 		        		
 						var currencyData = json;
-						var newRates = ((currencyData.bpi.GBP.rate_float)/(currencyData.bpi.EUR.rate_float)).toFixed(5);
-						// var lastRates;
+						var newRates = (currencyData.bpi.USD.rate_float);
 						$(element_id).text(newRates);
 
-						if(newRates >= lastRatesArray[4]){
+						if(newRates > lastRatesArray[2]){
 							$(element_id).css('color', '#09b83e');
 							// console.log("* " + newRates);
 						}
-						else if(newRates < lastRatesArray[4]){
+						else if(newRates < lastRatesArray[2]){
 							$(element_id).css('color', '#cd201f');
 							// console.log("* " + newRates);
 						}
-						lastRatesArray[4] = newRates;
+						lastRatesArray[2] = newRates;
 				});	  
 		        break;
+			// Rates API for EUR/USD
+		    case "#EURUSD":
+		    	var api = apiPath;
+
+		        $.getJSON(api).then(function(json){
+		        		
+						var currencyData = json;
+						var newRates = ((currencyData.bpi.USD.rate_float)/(currencyData.bpi.EUR.rate_float)).toFixed(5);
+						$(element_id).text(newRates);
+
+						if(newRates > lastRatesArray[3]){
+							$(element_id).css('color', '#09b83e');
+							// console.log("* " + newRates);
+						}
+						else if(newRates < lastRatesArray[3]){
+							$(element_id).css('color', '#cd201f');
+							// console.log("* " + newRates);
+						}
+						lastRatesArray[3] = newRates;
+				});	  
+		        break;
+			// 	// // Rates API for EUR/JPY
+			//  //    case "#EURJPY" & apiPath:
+			//  //        $.getJSON(apiPath).then(function(json){
+			        		
+			// 	// 			var currencyData = json;
+			// 	// 			// var lastRates;
+			// 	// 			var newRates = (currencyData.value);
+			// 	// 			$(element_id).text(newRates);
+
+			// 	// 			if(newRates >= lastRates){
+			// 	// 				$(element_id).css('color', '#09b83e');
+			// 	// 				// console.log("* " + newRates);
+			// 	// 			}
+			// 	// 			else if(newRates < lastRates){
+			// 	// 				$(element_id).css('color', '#cd201f');
+			// 	// 				// console.log("* " + newRates);
+			// 	// 			}
+			// 	// 			lastRates = newRates;
+			// 	// 	});	  
+			//  //        break;
+		    		// Rates API for EUR/GBP
+				    case "#EURGBP":
+			    	var api = apiPath;
+
+			        $.getJSON(api).then(function(json){
+			        		
+							var currencyData = json;
+							var newRates = ((currencyData.bpi.GBP.rate_float)/(currencyData.bpi.EUR.rate_float)).toFixed(5);
+							// var lastRates;
+							$(element_id).text(newRates);
+
+							if(newRates >= lastRatesArray[4]){
+								$(element_id).css('color', '#09b83e');
+								// console.log("* " + newRates);
+							}
+							else if(newRates < lastRatesArray[4]){
+								$(element_id).css('color', '#cd201f');
+								// console.log("* " + newRates);
+							}
+							lastRatesArray[4] = newRates;
+					});	  
+			        break;
 
 
-	    default:
-	        console.log("Something goes wrong in switch statement /set_rates.js");
+		    default:
+		        console.log("Something goes wrong in switch statement /set_rates.js");
+		}
 	}
-  }
 
 	set_rates("#EURRUB",apiPath1,"EUR","RUB");
 	set_rates("#USDRUB",apiPath1,"USD","RUB");
@@ -157,11 +157,11 @@ var lastRatesArray = [0,0,0,0,0,0,0,0];
 	}, 6000);
 
 	// Open - close form
-  $( "#Add" ).click(function() {
-  	$("#form").css("display", "block");
-  });
-    $( "#cancel" ).click(function() {
-  	$("#form").css("display", "none");
-  });
+	$( "#Add" ).click(function() {
+		$("#form").css("display", "block");
+	});
+	$( "#cancel" ).click(function() {
+		$("#form").css("display", "none");
+	});
 
 });
